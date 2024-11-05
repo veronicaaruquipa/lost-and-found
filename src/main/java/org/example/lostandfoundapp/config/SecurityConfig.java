817e64b9eb4dec8dcfc1afbbe372294fb77d424f
Veronica Aruquipa <veronica.aruquipa@vanderlande.com>
2024-11-06 00:07:32 +0100

    Adjust security config.

--- src/main/java/org/example/lostandfoundapp/config/SecurityConfig.java
+++ src/main/java/org/example/lostandfoundapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.lostandfoundapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +15,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll() // Allow all requests
                 )
-                .csrf(csrf -> csrf.disable()); // Disable CSRF protection
+                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection
 
         return http.build();
     }
