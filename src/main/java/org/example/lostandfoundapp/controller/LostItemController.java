24efe76b2ef97d174da9df092ebec2a6a2f7163b
Veronica Aruquipa <veronica.aruquipa@vanderlande.com>
2024-11-05 23:56:01 +0100

    Add controller and service.

---
+++ src/main/java/org/example/lostandfoundapp/controller/LostItemController.java
@@ -0,0 +1,67 @@
+package org.example.lostandfoundapp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.lostandfoundapp.dto.LostItemDTO;
+import org.example.lostandfoundapp.model.Claim;
+import org.example.lostandfoundapp.model.LostItem;
+import org.example.lostandfoundapp.service.LostItemService;
+import org.example.lostandfoundapp.util.PdfParserUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/lost-items")
+@Slf4j
+public class LostItemController {
+    private final LostItemService lostItemService;
+
+    public LostItemController(LostItemService lostItemService) {
+        this.lostItemService = lostItemService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        List<LostItem> items = readLostItemsFromPDF(file);
+        items.forEach(lostItemService::saveLostItem);
+        return ResponseEntity.ok("File uploaded and items saved successfully.");
+    }
+
+    @PostMapping
+    public LostItem createLostItem(@RequestBody LostItemDTO lostItemDTO) {
+        LostItem lostItem = new LostItem();
+        // Map lost item fields from DTO to entity
+        lostItem.setItemName(lostItemDTO.getItemName());
+        lostItem.setQuantity(lostItemDTO.getQuantity());
+        lostItem.setPlace(lostItemDTO.getPlace());
+        return lostItemService.saveLostItem(lostItem);
+    }
+
+    @GetMapping()
+    public List<LostItem> getAllLostItems() {
+        return lostItemService.getAllLostItems();
+    }
+
+    @PostMapping("/claim")
+    public ResponseEntity<Claim> claimItem(@RequestParam Long lostItemId, @RequestParam Long userId, @RequestParam int quantity) {
+        Claim claim = lostItemService.claimItem(lostItemId, userId, quantity);
+        return ResponseEntity.ok(claim);
+    }
+
+    @GetMapping("/claims")
+    public ResponseEntity<List<Claim>> getAllClaims() {
+        List<Claim> claims = lostItemService.getAllClaims();
+        return ResponseEntity.ok(claims);
+    }
+
+    private List<LostItem> readLostItemsFromPDF(MultipartFile file) {
+        return PdfParserUtil.parseFile(file);
+    }
+}
