e07159a3887d78d12a3348f4b34edd950cde2c27
Veronica Aruquipa <veronica.aruquipa@vanderlande.com>
2024-11-06 00:00:08 +0100

    Add exception handlers and security config.

---
+++ src/main/java/org/example/lostandfoundapp/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.example.lostandfoundapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Allow all requests
+                )
+                .csrf(csrf -> csrf.disable()); // Disable CSRF protection
+
+        return http.build();
+    }
+}
