18329a57b29a57a0dd488386be61621fa86086b9
Veronica Aruquipa <veronica.aruquipa@vanderlande.com>
2024-11-05 23:14:12 +0100

    Define entities.

---
+++ src/main/java/org/example/lostandfoundapp/model/Claim.java
@@ -0,0 +1,34 @@
+package org.example.lostandfoundapp.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+public class Claim {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "User ID is mandatory")
+    private Long userId;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "lost_item_id", nullable = false)
+    @JsonBackReference
+    private LostItem lostItem;
+}
